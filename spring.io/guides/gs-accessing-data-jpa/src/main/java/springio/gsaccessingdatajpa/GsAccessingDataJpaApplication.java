0a403d629cd22682e2bbe17f5bc0b6ab657b0800
George Panaretos <georgepanaretos.cac@gmail.com>
2020-11-17 10:52:55 +0100

    Guide Accessing Data with JPA

---
+++ spring.io/guides/gs-accessing-data-jpa/src/main/java/springio/gsaccessingdatajpa/GsAccessingDataJpaApplication.java
@@ -0,0 +1,13 @@
+package springio.gsaccessingdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccessingDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAccessingDataJpaApplication.class, args);
+	}
+
+}
---
+++ spring.io/guides/gs-accessing-data-jpa/src/test/java/springio/gsaccessingdatajpa/GsAccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package springio.gsaccessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
