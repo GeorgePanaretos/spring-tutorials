269a55a84b62f014060b1c3f9b223a1ccdec55ce
George Panaretos <georgepanaretos.cac@gmail.com>
2023-01-02 13:09:44 +0100

    Clean up of Accessing Data with JPA guide

--- spring.io/guides/gs-accessing-data-jpa/src/main/java/example/AccessingDataJpaApplication.java
+++ spring.io/guides/gs-accessing-data-jpa/src/main/java/example/AccessingDataJpaApplication.java
@@ -47,9 +47,6 @@ public class AccessingDataJpaApplication {
             repository.findByLastName("Bauer").forEach(bauer -> {
                 log.info(bauer.toString());
             });
-            // for (Customer bauer : repository.findByLastName("Bauer")) {
-            //  log.info(bauer.toString());
-            // }
             log.info("");
         };
     }
